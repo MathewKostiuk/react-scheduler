00b5f3eb5b8b64447303643745b254d8d163cf18
Mathew Kostiuk <mathewkostiuk@gmail.com>
2019-11-10 23:11:45 +0900

    implemented the transtions for the Empty, Show, and Create modes for the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,19 +4,50 @@ import "./style.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import useVisualMode from '../../hooks/useVisualMode';
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
+
+
 
 export default function Appointment ({
   time,
   id,
-  interview
+  interview,
 }) {
+
+  const { mode, transition, back} = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  const onAdd = () => {
+    transition(CREATE);
+  }
+
+  const onCancel = () => {
+    back();
+  }
+
   return (
     <article id={id} className="appointment">
       <Header time={time}/>
-      {interview ? (
-        <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty/>
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={onCancel}
+        />
       )}
     </article>
   )
