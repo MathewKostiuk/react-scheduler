1a3f19e8408a75032a9ef2cb71e4217fb72950c7
Mathew Kostiuk <mathewkostiuk@gmail.com>
2019-11-18 13:39:02 +0900

    Implemented useReducer for better complex state management

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,61 +1,69 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
 
-  const [state, setState] = useState({
+  const initialState = {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
-  });
+  }
 
-  const setDay = day => setState({ ...state, day });
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+
+  const reducers = {
+    [SET_DAY]: (state, { day }) => {
+      return { ...state, day }
+    },
+    [SET_APPLICATION_DATA]: (state, {days, appointments, interviewers}) => {
+      return { ...state, days, appointments, interviewers }
+    },
+    [SET_INTERVIEW]: (state, { id, interview }) => {
+      const appointment = {
+        ...state.appointments[id],
+        interview: interview
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+      console.log(state, { id, interview });
+      return { ...state, appointments }
+    }
+  }
+
+  function reducer(state, action) {
+    return reducers[action.type](state, action) || state;
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setDay = day => dispatch({ type: SET_DAY, day });
 
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
+    ]).then(([days, appointments, interviewers]) => {
+      dispatch({ type: SET_APPLICATION_DATA, days: days.data, appointments: appointments.data, interviewers: interviewers.data });
     })
   }, []);
 
   async function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    }
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
 
     await axios.put(`/api/appointments/${id}`, { interview });
-    setState({
-      ...state,
-      appointments
-    });
+    dispatch({ type: SET_INTERVIEW, id, interview });
   }
 
   async function deleteInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
 
     await axios.delete(`/api/appointments/${id}`);
-    setState({
-      ...state,
-      appointments
-    });
+    dispatch({ type: SET_INTERVIEW, id, interview: null });
   }
 
   return { state, setDay, bookInterview, deleteInterview };
